9e7dded17bed4d176ab6a98cb67a3562bd5d1697
weykon <635883092@qq.com>
2023-03-30 17:12:48 +0800

    fix export

--- ai_say.ts
+++ ai_say.ts
@@ -1,46 +1,38 @@
 import { Configuration, OpenAIApi, CreateCompletionRequest } from "openai";
-import env from 'dotenv';
-env.config();
 
-const OPENAI_CHATGPT_KEY = process.env.OPENAI_CHATGPT_KEY;
-const ORGANIZATION = process.env.ORGANIZATION;
-
-const configuration = new Configuration({
-    organization: ORGANIZATION,
-    apiKey: OPENAI_CHATGPT_KEY,
-});
-
-export const myopenai = new OpenAIApi(configuration);
+export const aisay = new OpenAIApi();
+type Option = {
+    organization: string,
+    apiKey: string,
+}
 
 export class OpenAIAPI {
     api: OpenAIApi
-    config: Configuration
-    constructor() {
-        this.config = new Configuration({
-            organization: ORGANIZATION,
-            apiKey: OPENAI_CHATGPT_KEY,
-        });
-        this.api = new OpenAIApi(configuration);
+    #config: Configuration
+    config(option: Option) {
+        const { organization, apiKey } = option;
+        this.#config = new Configuration({ organization, apiKey });
+        this.api = new OpenAIApi(this.#config);
     }
 }
 
-async function chat(prompt: string, model: string, length: number) {
-    const response = await myopenai.createChatCompletion({
+export async function chat(prompt: string, model: string, length: number) {
+    const response = await aisay.createChatCompletion({
         messages: [{ role: 'user', content: prompt }],
         model: model,
     });
     return response
 }
 
-async function one_ask(option: CreateCompletionRequest) {
-    const response: any = await myopenai.createCompletion({
+export async function one_ask(option: CreateCompletionRequest) {
+    const response: any = await aisay.createCompletion({
         ...option
     });
     return response.data?.choices?.[0].text.trim();
 }
 
-async function createEmbedding(prompt: string, embModel: string = 'text-embedding-ada-002') {
-    const response = await myopenai.createEmbedding({
+export async function createEmbedding(prompt: string, embModel: string = 'text-embedding-ada-002') {
+    const response = await aisay.createEmbedding({
         model: embModel,
         input: [prompt],
     });
